f3548f3cae8fffc72f94119a0524b51fbedd4ac3
Alex Cheema <alexcheema123@gmail.com>
2023-05-24 13:47:40 +0100

    get transactions over rpc

--- scraper/index.js
+++ scraper/index.js
@@ -4,7 +4,11 @@ import { FileCursor } from './cursor.js';
 const rpcEndpoint = process.env.RPC_ENDPOINT || 'https://rpc.testnet.mantle.xyz';
 
 async function processBlocks(blocks) {
-
+  const txHashes = blocks.reduce((a, b) => ([...a, ...b.transactions]), []);
+  console.log(txHashes);
+  const txs = await rpc.getTransactions(rpcEndpoint, txHashes);
+  console.log(JSON.stringify(txs, null, 2));
+  console.log(JSON.stringify(blocks, null, 2));
 }
 
 async function run() {
@@ -21,6 +25,7 @@ async function run() {
       const blocks = await rpc.scrapeBlocks(rpcEndpoint, cursorBlockNumber + 1, nextBlockNumberTo);
       const blockNumbers = blocks.map(b => parseInt(b.number, 16));
       console.log(`Scraped ${blocks.length} blocks!`, Math.min(...blockNumbers), Math.max(...blockNumbers));
+      await processBlocks(blocks);
       await cursor.save(nextBlockNumberTo);
       cursorBlockNumber = nextBlockNumberTo;
     } catch (err) {
--- scraper/rpc.js
+++ scraper/rpc.js
@@ -17,6 +17,33 @@ export async function scrapeBlocks(rpcEndpoint, startBlock, endBlock = 'latest')
   return resp.data.result;
 }
 
+export async function getTransactions(rpcEndpoint, txHashes) {
+  if (txHashes.length > 1000) throw new Error('max batch req is 1000');
+  const resp = await axios.post(rpcEndpoint, txHashes.map(txHash => ({
+    jsonrpc: "2.0",
+    method: "eth_getTransactionByHash",
+    params: [txHash],
+    id: 1,
+  })));
+
+  if (!resp.data) throw new Error(`no result in response: ${JSON.stringify(resp.data)}`);
+
+  return resp.data.map(res => res.result);
+}
+
+export async function getTransaction(rpcEndpoint, txHash) {
+  const resp = await axios.post(rpcEndpoint, {
+    jsonrpc: "2.0",
+    method: "eth_getTransactionByHash",
+    params: [txHash],
+    id: 1,
+  });
+
+  if (!resp.data?.result) throw new Error(`no result in response: ${JSON.stringify(resp.data)}`);
+
+  return resp.data.result;
+}
+
 export async function getLatestIndexedBlockNumber(rpcEndpoint) {
   const resp = await axios.post(rpcEndpoint, {
     jsonrpc: "2.0",
