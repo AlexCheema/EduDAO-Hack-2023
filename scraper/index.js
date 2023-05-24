2e2f31fde2cf84a2054c5483855966d046af1226
Alex Cheema <alexcheema123@gmail.com>
2023-05-24 13:47:39 +0100

    scrape all blocks up to the latest using a cursor to keep track of where you left off

--- scraper/index.js
+++ scraper/index.js
@@ -1,12 +1,28 @@
 import * as rpc from './rpc.js';
+import { FileCursor } from './cursor.js';
 
 const rpcEndpoint = process.env.RPC_ENDPOINT || 'https://rpc.testnet.mantle.xyz';
 
 async function run() {
+  const cursor = FileCursor('cursor.json', 0);
+
   const latestBlockNumber = await rpc.getLatestIndexedBlockNumber(rpcEndpoint);
   console.log({ latestBlockNumber });
-  const blocks = await rpc.scrapeBlocks(rpcEndpoint, latestBlockNumber - 100, latestBlockNumber);
-  console.log({ blocks: JSON.stringify(blocks, null, 2) });
+
+  let cursorBlockNumber = await cursor.get();
+  const blockNumberBatchSize = 1000;
+  while (cursorBlockNumber < latestBlockNumber) {
+    const nextBlockNumberTo = Math.min(cursorBlockNumber + blockNumberBatchSize, latestBlockNumber);
+    try {
+      const blocks = await rpc.scrapeBlocks(rpcEndpoint, cursorBlockNumber + 1, nextBlockNumberTo);
+      console.log(`Scraped ${blocks.length} blocks!`, JSON.stringify(blocks.map(b => b.number)));
+      await cursor.save(nextBlockNumberTo);
+      cursorBlockNumber = nextBlockNumberTo;
+    } catch (err) {
+      console.error(err);
+      console.log(`error so will try again from block ${cursorBlockNumber} to ${nextBlockNumberTo}`);
+    }
+  }
 }
 
 await run();
