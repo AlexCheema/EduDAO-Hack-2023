dd6e21dd9143d0856cb057a004f6d89ffb8112e5
Alex Cheema <alexcheema123@gmail.com>
2023-05-24 13:47:39 +0100

    import all as rpc to be more explicit with rpc calls

--- scraper/index.js
+++ scraper/index.js
@@ -1,11 +1,11 @@
-import { scrapeBlocks, getLatestIndexedBlockNumber } from './rpc.js';
+import * as rpc from './rpc.js';
 
 const rpcEndpoint = 'https://rpc.testnet.mantle.xyz' || process.env.RPC_ENDPOINT;
 
 async function run() {
-  const latestBlockNumber = await getLatestIndexedBlockNumber(rpcEndpoint);
+  const latestBlockNumber = await rpc.getLatestIndexedBlockNumber(rpcEndpoint);
   console.log({ latestBlockNumber });
-  const blocks = await scrapeBlocks(rpcEndpoint, latestBlockNumber - 100, latestBlockNumber);
+  const blocks = await rpc.scrapeBlocks(rpcEndpoint, latestBlockNumber - 100, latestBlockNumber);
   console.log({ blocks: JSON.stringify(blocks, null, 2) });
 }
 
