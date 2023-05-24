9a69f4005d082335441239cbc2308151f186202c
Alex Cheema <alexcheema123@gmail.com>
2023-05-24 13:47:40 +0100

    s3 for cursor and block storage

--- scraper/cursor.js
+++ scraper/cursor.js
@@ -1,4 +1,5 @@
 import { promises as fs } from 'fs';
+import { PutObjectCommand, GetObjectCommand } from '@aws-sdk/client-s3';
 
 export function FileCursor(fileName = 'cursor.json', defaultValue = 0) {
   return {
@@ -9,7 +10,7 @@ export function FileCursor(fileName = 'cursor.json', defaultValue = 0) {
       try {
         const data = await fs.readFile(fileName);
         if (!data) return defaultValue;
-        return JSON.parse(data).cursor ?? defaultValue;
+        return JSON.parse(data)?.cursor ?? defaultValue;
       } catch (err) {
         return defaultValue;
       }
@@ -17,4 +18,24 @@ export function FileCursor(fileName = 'cursor.json', defaultValue = 0) {
   }
 }
 
+export function S3Cursor(s3, bucketName, key = 'cursor.json', defaultValue = 0) {
+  return {
+    save: async function(cursor) {
+      await s3.send(new PutObjectCommand({
+        Bucket: bucketName, Key: key, Body: JSON.stringify({ cursor })
+      }));
+    },
+    get: async function() {
+      try {
+        const result = await s3.send(new GetObjectCommand({
+          Bucket: bucketName, Key: key
+        }));
+        if (!result?.getObjectResult?.Body) return defaultValue;
+        return JSON.parse(result.getObjectResult.Body.transformToString())?.cursor ?? defaultValue;
+      } catch (err) {
+        return defaultValue;
+      }
+    },
+  }
+}
 
--- scraper/index.js
+++ scraper/index.js
@@ -1,7 +1,13 @@
 import * as rpc from './rpc.js';
-import { FileCursor } from './cursor.js';
+import { FileCursor, S3Cursor } from './cursor.js';
+import { S3 } from '@aws-sdk/client-s3';
+import { PutObjectCommand, GetObjectCommand } from '@aws-sdk/client-s3';
 
-const rpcEndpoint = process.env.RPC_ENDPOINT || 'https://rpc.testnet.mantle.xyz';
+const rpcEndpoint = process.env.RPC_ENDPOINT ?? 'https://rpc.testnet.mantle.xyz';
+const useS3 = !!process.env.USE_S3;
+const s3 = new S3({});
+const s3BucketName = process.env.S3_BUCKET_NAME;
+const s3CursorKey = process.env.S3_CURSOR_KEY ?? 'cursor.json';
 
 async function processBlocks(blocks) {
   const txHashes = blocks.reduce((a, b) => ([...a, ...b.transactions]), []);
@@ -9,10 +15,23 @@ async function processBlocks(blocks) {
   const txs = await rpc.getTransactions(rpcEndpoint, txHashes);
   console.log(JSON.stringify(txs, null, 2));
   console.log(JSON.stringify(blocks, null, 2));
+
+  const blockNumbers = blocks.map(b => parseInt(b.number, 16));
+  const min = Math.min(...blockNumbers);
+  const max = Math.max(...blockNumbers);
+  console.log(`Scraped ${blocks.length} blocks from ${min} to ${max}!`);
+  if (!useS3) return;
+
+  const key = `blocks/${min}_${max}.json`;
+  await s3.send(new PutObjectCommand({
+    Bucket: s3BucketName, Key: key, Body: JSON.stringify(blocks)
+  }));
 }
 
 async function run() {
-  const cursor = FileCursor('cursor.json', 0);
+  const cursor = useS3 ?
+    S3Cursor(s3, s3BucketName, s3CursorKey) :
+    FileCursor('cursor.json', 0);
 
   const latestBlockNumber = await rpc.getLatestIndexedBlockNumber(rpcEndpoint);
   console.log({ latestBlockNumber });
@@ -23,8 +42,11 @@ async function run() {
     const nextBlockNumberTo = Math.min(cursorBlockNumber + blockNumberBatchSize, latestBlockNumber);
     try {
       const blocks = await rpc.scrapeBlocks(rpcEndpoint, cursorBlockNumber + 1, nextBlockNumberTo);
+<<<<<<< HEAD
       const blockNumbers = blocks.map(b => parseInt(b.number, 16));
       console.log(`Scraped ${blocks.length} blocks!`, Math.min(...blockNumbers), Math.max(...blockNumbers));
+=======
+>>>>>>> s3 for cursor and block storage
       await processBlocks(blocks);
       await cursor.save(nextBlockNumberTo);
       cursorBlockNumber = nextBlockNumberTo;
